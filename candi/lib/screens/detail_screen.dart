6f85018ccc32f07b0802206f8410a4f4f5e2969e
Cesya <dealyalti_2226240037@mhs.mdp.ac.id>
2024-11-13 17:19:28 +0700

    comit 1

---
+++ candi/lib/screens/detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class DetailScreen extends StatelessWidget{
+  final Candi candi; 
+
+  const DetailScreen({super.key, required this.candi});
+
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    )
+  }
+}
